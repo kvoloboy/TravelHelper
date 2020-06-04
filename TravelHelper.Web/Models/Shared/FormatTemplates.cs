231a77ee0a8112769750640ea0335035b811d74b
Vovchikz <volodymyr.sorokin@nure.ua>
2020-06-05 00:30:38 +0300

    Added models.shared, wwwroot,views

---
+++ TravelHelper.Web/Models/Shared/FormatTemplates.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelHelper.Web.Models.Shared
+{
+    public static class FormatTemplates
+    {
+        public const string CurrencyFormat = "C2";
+    }
+}
