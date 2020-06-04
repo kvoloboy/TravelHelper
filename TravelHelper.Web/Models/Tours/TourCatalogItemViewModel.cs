ab04bdf91adc8b5da8d29fcd7f6a6ebd75c47a4d
kyrylo_voloboi <kyrylo_voloboi@epam.com>
2020-06-04 17:03:21 +0300

    Added seed views

--- TravelHelper.Web/Models/Tours/TourCatalogItemViewModel.cs
+++ TravelHelper.Web/Models/Tours/TourCatalogItemViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace TravelHelper.Web.Models.Tours
+﻿using TravelHelper.Web.Models.Images;
+
+namespace TravelHelper.Web.Models.Tours
 {
     public class TourCatalogItemViewModel
     {
-        
+        public string Name { get; set; }
+        public string Description{ get; set; }
+        public ImageViewModel Image { get; set; }
     }
 }
--- TravelHelper.Web/Models/Tours/TourCatalogViewModel.cs
+++ TravelHelper.Web/Models/Tours/TourCatalogViewModel.cs
@@ -3,7 +3,7 @@ using TravelHelper.Web.Models.Tours.Filters;
 
 namespace TravelHelper.Web.Models.Tours
 {
-    public class CatalogViewModel
+    public class TourCatalogViewModel
     {
         public FilterViewModel Filter { get; set; }
         public IEnumerable<TourCatalogItemViewModel> Items { get; set; }
